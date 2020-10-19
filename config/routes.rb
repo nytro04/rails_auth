894332b948364bf108cfa52002759c8f59d16b24
Francis Badasu <nytro04@gmail.com>
2020-10-19 03:45:52 +0000

    feat: add routes and application controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'pages#index'
+
+  resources :users, only: [:new, :create]
+
+  resources :user_sessions, only: [:create, :destory]
+
+  delete '/sign_out', to: 'user_session#destroy', as: :sign_out
+  get '/sign_in', to:'user_session#new', as: :sign_in
+
+  
 end
